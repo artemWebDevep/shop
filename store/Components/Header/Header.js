af8ef28cd8fff8ee3b760fbe25eeb7ba67ecd1e0
Artem Shalamov <iart1756@gmail.com>
2020-04-16 16:03:17 +0500

    create store

---
+++ store/Components/Header/Header.js
@@ -0,0 +1,47 @@
+class Header {
+    
+    handlerOpenShoppingPage(){
+        shoppingPage.render()
+    }
+
+    render(count) {
+        const html = `
+        <div class="navbar-fixed">
+          <nav>
+          <div class="nav-wrapper">
+            <img class="shop" src="./static/images/Icons/porridge.svg" alt="">
+            <a href="index.html" class="brand-logo"><Logo>ГречкаЕд</a>
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <ul class="right hide-on-med-and-down">
+              <li>
+              <a onclick="headerPage.handlerOpenShoppingPage();">Корзина ${count}шт. </a>
+              </li>
+              <li><a href="#">Доставка</a></li>
+              <li><a href="#">Пункты выдачи</a></li>
+              <li><a href="#">Помощь</a></li>
+              <li><a href="#">О нас</a></li>
+              
+            </ul>
+          </div>
+        </nav>
+      </div>
+    
+      <ul class="sidenav" id="mobile-demo">
+        <li><a onclick="headerPage.handlerOpenShoppingPage();">Корзина ${count}</a></li>
+        <li><a href="#">Доставка</a></li>
+        <li><a href="#">Пункты выдачи</a></li>
+        <li><a href="#">Помощь</a></li>
+        <li><a href="#">О нас</a></li>
+      </ul>
+        `;
+        ROOT_HEADER.innerHTML = html;
+    }
+}
+
+const headerPage = new Header()
+
+const productStore = localStorageUtil.getProducts()
+
+
+
+headerPage.render(productStore.length)
---
+++ store/constants/catalog.js
@@ -0,0 +1,63 @@
+const CATALOG = [
+
+    {
+        "id": "el1",
+        "name": "Гречка",
+        "img": "https://sc01.alicdn.com/kf/UTB8ELZgGSnEXKJk43Ubq6zLppXa3/Roasted-Buckwheat.jpg",
+        "price": 79
+    },
+    {
+        "id": "el2",
+        "name": "Рис",
+        "img": "https://chistyulka.ru/wp-content/uploads/2019/03/long-grain-rice-kst-dubai-1-800x600.png",
+        "price": 79
+    },
+    {
+        "id": "el3",
+        "name": "Пшено",
+        "img": "http://m.zakroma10.ru/assets/Uploads/b0db6690b62072ddb6b57aa84524c6d1.jpeg",
+        "price": 59
+    },
+    {
+        "id": "el4",
+        "name": "Туалетная бумага",
+        "img": "https://titanhim.ru/wa-data/public/shop/products/25/12/1225/images/1240/1240.970.jpg",
+        "price": 29
+    },
+    {
+        "id": "el5",
+        "name": "Спички",
+        "img": "https://flytothesky.ru/wp-content/uploads/2018/02/65437.jpg",
+        "price": 10
+    },
+    {
+        "id": "el6",
+        "name": "Мыло",
+        "img": "https://kazanexpress.hb.bizmrg.com/bnenrqfpe0ss1kp10sp0/original.jpg",
+        "price": 25
+    },
+    {
+        "id": "el7",
+        "name": "Сахар",
+        "img": "https://sc01.alicdn.com/kf/UTB8rxJQIJoSdeJk43Owq6ya4XXa6/REFINED-BEET-SUGAR-BRAZILIAN-REFINED-WHITE-CANE.jpg",
+        "price": 32
+    },
+    {
+        "id": "el8",
+        "name": "Сгущеное молоко",
+        "img": "https://gekomarket.com/uploads/products/5a7ac427870cb.jpg",
+        "price": 55
+    },
+    {
+        "id": "el9",
+        "name": "Тушенка",
+        "img": "https://cdn.metro-cc.ru/ru/ru_pim_544188001001_01.png",
+        "price": 69
+    },
+    {
+        "id": "el10",
+        "name": "Бензин",
+        "img": "https://media2.24aul.ru/imgs/5d147f42604e9c0001cac661/",
+        "price": 41.60
+    }
+]
